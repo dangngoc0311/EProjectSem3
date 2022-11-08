5d03a6e3a6478aea1e7cc4881c7874fa5422c319
Ngọc Đặng Thị Bích <ngoc1233456789@gmail.com>
2022-11-08 08:32:46 +0000

    Add view component

---
+++ LaundryOnline/ViewComponents/ServicesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using LaundryOnline.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaundryOnline.ViewComponents
+{
+    public class ServicesViewComponent:ViewComponent
+    {
+        private readonly LaundryOnlineContext _context;
+        public ServicesViewComponent(LaundryOnlineContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var services = _context.Services;
+            return View(services);
+        }
+    }
+}
