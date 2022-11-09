88d2090c873f21f314cc8fd63dd5f87c32246d4d
Minh Hoàng <minhhoang140802@gmail.com>
2022-11-09 03:15:35 +0000

    Admin units

--- LaundryOnline/Areas/Admin/Controllers/UnitsController.cs
+++ LaundryOnline/Areas/Admin/Controllers/UnitsController.cs
@@ -8,35 +8,34 @@ using Microsoft.EntityFrameworkCore;
 using LaundryOnline.Models;
 using NToastNotify;
 using X.PagedList;
+
 namespace LaundryOnline.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class ServicesController : Controller
+    public class UnitsController : Controller
     {
         private readonly LaundryOnlineContext _context;
         private readonly IToastNotification _toastNotification;
-
-        public ServicesController(LaundryOnlineContext context, IToastNotification toastrNotification)
+        public UnitsController(LaundryOnlineContext context, IToastNotification toastrNotification)
         {
             _context = context;
             _toastNotification = toastrNotification;
         }
 
-        // GET: Admin/Services
+        // GET: Admin/Units
         public async Task<IActionResult> Index(int? page, string name)
         {
             page = page ?? 1;
             int pageSize = 6;
-            var services =_context.Services.AsQueryable();
+            var unit = _context.Units.Include(x => x.Service).AsQueryable();
             if (!string.IsNullOrEmpty(name))
             {
-                services = services.Where(j => j.ServiceName.Contains(name));
+                unit = unit.Where(j => j.UnitName.Contains(name));
             }
-            
-            return View(await services.ToPagedListAsync(page, pageSize));
+            return View(await unit.ToPagedListAsync(page, pageSize));
         }
 
-        // GET: Admin/Services/Details/5
+        // GET: Admin/Units/Details/5
         public async Task<IActionResult> Details(string id)
         {
             if (id == null)
@@ -44,41 +43,44 @@ namespace LaundryOnline.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var service = await _context.Services
-                .FirstOrDefaultAsync(m => m.ServiceId == id);
-            if (service == null)
+            var unit = await _context.Units
+                .Include(u => u.Service)
+                .FirstOrDefaultAsync(m => m.UnitId == id);
+            if (unit == null)
             {
                 return NotFound();
             }
 
-            return View(service);
+            return View(unit);
         }
 
-        // GET: Admin/Services/Create
+        // GET: Admin/Units/Create
         public IActionResult Create()
         {
+            ViewData["ServiceName"] = new SelectList(_context.Services, "ServiceId", "ServiceName");
             return View();
         }
 
-        // POST: Admin/Services/Create
+        // POST: Admin/Units/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ServiceId,ServiceName,ServiceDescription")] Service service)
+        public async Task<IActionResult> Create([Bind("UnitId,UnitName,UnitPrice,ServiceId")] Unit unit)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(service);
+                _context.Add(unit);
                 await _context.SaveChangesAsync();
                 _toastNotification.AddSuccessToastMessage("Create new service successfully");
                 return RedirectToAction(nameof(Index));
             }
             _toastNotification.AddErrorToastMessage("Create new service failed");
-            return View(service);
+            ViewData["ServiceId"] = new SelectList(_context.Services, "ServiceId", "ServiceId", unit.ServiceId);
+            return View(unit);
         }
 
-        // GET: Admin/Services/Edit/5
+        // GET: Admin/Units/Edit/5
         public async Task<IActionResult> Edit(string id)
         {
             if (id == null)
@@ -86,22 +88,23 @@ namespace LaundryOnline.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var service = await _context.Services.FindAsync(id);
-            if (service == null)
+            var unit = await _context.Units.FindAsync(id);
+            if (unit == null)
             {
                 return NotFound();
             }
-            return View(service);
+            ViewData["Service"] = new SelectList(_context.Services, "ServiceId", "ServiceName", unit.ServiceId);
+            return View(unit);
         }
 
-        // POST: Admin/Services/Edit/5
+        // POST: Admin/Units/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("ServiceId,ServiceName,ServiceDescription")] Service service)
+        public async Task<IActionResult> Edit(string id, [Bind("UnitId,UnitName,UnitPrice,ServiceId")] Unit unit)
         {
-            if (id != service.ServiceId)
+            if (id != unit.UnitId)
             {
                 return NotFound();
             }
@@ -110,13 +113,13 @@ namespace LaundryOnline.Areas.Admin.Controllers
             {
                 try
                 {
-                    _context.Update(service);
+                    _context.Update(unit);
                     await _context.SaveChangesAsync();
                     _toastNotification.AddSuccessToastMessage("Update successfully");
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ServiceExists(service.ServiceId))
+                    if (!UnitExists(unit.UnitId))
                     {
                         return NotFound();
                     }
@@ -127,31 +130,51 @@ namespace LaundryOnline.Areas.Admin.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(service);
+            ViewData["ServiceId"] = new SelectList(_context.Services, "ServiceId", "ServiceId", unit.ServiceId);
+            return View(unit);
         }
 
-        // GET: Admin/Services/Delete/5
+        // GET: Admin/Units/Delete/5
         public async Task<IActionResult> Delete(string id)
         {
-            var service = await _context.Services.FindAsync(id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var unit = await _context.Units
+                .Include(u => u.Service)
+                .FirstOrDefaultAsync(m => m.UnitId == id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+
+            return View(unit);
+        }
+
+        // POST: Admin/Units/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var unit = await _context.Units.FindAsync(id);
             try
             {
-                _context.Services.Remove(service);
+                _context.Units.Remove(unit);
                 await _context.SaveChangesAsync();
-                _toastNotification.AddSuccessToastMessage("Delete service " + service.ServiceName + " successfully");
+                _toastNotification.AddSuccessToastMessage("Delete service " + unit.UnitName + " successfully");
             }
             catch (Exception)
             {
-                _toastNotification.AddErrorToastMessage("Delete service " + service.ServiceName + " failed");
+                _toastNotification.AddErrorToastMessage("Delete service " + unit.UnitName + " failed");
             }
-          
             return RedirectToAction(nameof(Index));
         }
 
-
-        private bool ServiceExists(string id)
+        private bool UnitExists(string id)
         {
-            return _context.Services.Any(e => e.ServiceId == id);
+            return _context.Units.Any(e => e.UnitId == id);
         }
     }
 }
