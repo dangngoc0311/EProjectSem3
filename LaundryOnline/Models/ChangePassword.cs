9f0019b2d6d68c379f18c65923651a2c2d84f21c
Minh Hoàng <minhhoang140802@gmail.com>
2022-11-23 04:52:06 +0000

    Quan ly tai khoan

---
+++ LaundryOnline/Models/ChangePassword.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaundryOnline.Models
+{
+    public class ChangePassword
+    {
+        [DisplayName("User Id")]
+        public string UserId { get; set; }
+
+        [DisplayName("Password")]
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [DisplayName("ConfirmPassword")]
+        [Required]
+        [Compare("Password", ErrorMessage = "Password and Confirmation Password must match.")]
+        [MinLength(6)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
